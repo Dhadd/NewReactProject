5ccbd02722c619afa2a41474985fd86e7d00d5ae
Chetan Dhadd <rbkchetan143@gmail.com>
2021-12-24 22:44:10 -0800

    Iniaal Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Chetan from './components/Chetan';
+
+function App() {
+  return (
+    <div className="App">
+     
+      <Chetan />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Chetan.js
@@ -0,0 +1,21 @@
+// import React from "react";
+// function Chetan() {
+//     return (
+//         <div>
+//             <h1>Hello World</h1>
+//         </div>
+//     )
+// }
+
+import React from 'react'
+
+const Chetan = () => {
+    return (
+        <div>
+            <h1 style={{color:"black"}}>Hello World </h1>
+            <h1>New React Project</h1>
+        </div>
+    )
+}
+
+export default Chetan
