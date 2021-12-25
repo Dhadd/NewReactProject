2964a6b277fe3ee568595cd48ff831797d66093a
Chetan Dhadd <rbkchetan143@gmail.com>
2021-12-24 23:11:25 -0800

    New file added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Chetan from './components/Chetan';
+import New from './components/New';
 
 function App() {
   return (
     <div className="App">
+      <New />
      
       <Chetan />
     </div>
